c4b0c561dfc804e6801769e3617e20aa0def2b19
SimhadriRaviTeja <92438213+SimhadriRaviTeja@users.noreply.github.com>
2021-10-14 18:45:30 +0530

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CreateEmployeeComponent,
+    EmployeeDetailsComponent,
+    UpdateEmployeeComponent,
+    EmployeeListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
